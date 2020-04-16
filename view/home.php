44d7df927eb07fe4af253cf9623df79c85f26319
aurelien3333 <aurelienverdeau@gmail.com>
2020-04-16 12:41:55 +0200

    Création view home page, création routeur index.php création controller.php création model: CommentManager, PostManager, Manager

--- view/home.php
+++ view/home.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <title>Mon blog</title>
-    <link href="style.css" rel="stylesheet" />
+    <link href="../public/css/style.css" rel="stylesheet" />
 </head>
 
 <body>
