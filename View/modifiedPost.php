60afa76ae9c5d342fea99a6cde2031503ca1fac7
aurelien3333 <aurelienverdeau@gmail.com>
2020-05-08 16:03:14 +0200

    Correction de bug sur le design et le slud des titres des articles

--- View/modifiedPost.php
+++ View/modifiedPost.php
@@ -3,20 +3,14 @@
 <?php ob_start(); ?>
 
 
-<form action="/updatePost/<?= $post->getId();?>" method="post">
-    <label for="title_post">Titre</label>
-    <br />
-    <input type="text" id="title_post" name="title_post" value="<?= $post->getTitle();?>">
-    <br />
-    <label for="author_post">Auteur</label>
-    <br />
-    <input type="text" id="author_post" name="author_post" value="<?= $post->getAuthor();?>">
-    <br />
-    <label for="content_post">Redigez votre article</label>
-    <br />
+<form action="/updatePost/<?= $post->getId();?>" method="post" class="addpost">
+    <label for="title_post" class="addpost__label">Titre de l'article</label>
+    <input type="text" id="title_post" name="title_post" class="addpost__input" value="<?= $post->getTitle();?>" required>
+    <label for="author_post" class="addpost__label">Auteur</label>
+    <input type="text" id="author_post" name="author_post" class="addpost__input" value="<?= $post->getAuthor();?>" required>
+    <label for="content_post" class="addpost__label">Redigez votre article</label>
     <textarea id="mce" name="content_post"><?= $post->getContent();?></textarea>
-    <br />
-    <button type="submit">Publier</button>
+    <button type="submit" class="addpost__button">Modifier</button>
 </form>
 
 <script>
--- View/readPost.php
+++ View/readPost.php
@@ -7,9 +7,9 @@
 <form action="/addPost" method="post" class="addpost">
 
     <label for="title_post" class="addpost__label">Titre de l'article</label>
-    <input type="text" id="title_post" name="title_post" class="addpost__input">
+    <input type="text" id="title_post" name="title_post" class="addpost__input" required>
     <label for="author_post" class="addpost__label">Auteur</label>
-    <input type="text" id="author_post" name="author_post" class="addpost__input">
+    <input type="text" id="author_post" name="author_post" class="addpost__input" required>
     <label for="content_post" class="addpost__label">Redigez votre article</label>
     <textarea id="mce" name="content_post"></textarea>
     <button type="submit" class="addpost__button">Publier</button>
